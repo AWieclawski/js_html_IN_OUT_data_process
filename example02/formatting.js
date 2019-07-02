a0a80a627803da8279e5778b12286af576722c88
AWieclawski <a_wieclawski@poczta.pl>
2019-07-02 15:01:33 +0200

    Added text decoration & transform

--- example02/formatting.js
+++ example02/formatting.js
@@ -4,11 +4,13 @@ function styleFormat(x)
 	var size = document.getElementById('size').value;
 	var style = document.getElementById('style').value;
 	var weight = document.getElementById('weight').value;
+	var transform = document.getElementById('transform').value;
+	var decoration = document.getElementById('decoration').value;
 	var color = String(x);
 
-	console.log("size: ",size,"style: ",style,"color: ",color,"weight: "+weight);
+	console.log("size: ",size,", style: ",style,", color: ",color,", weight: "+weight,", transform: ",transform,", decoration: ",decoration);
 
-	styleParameters = 'font-size:'+size+'%;font-style:'+style+';color:'+color+';font-weight:'+weight+';';
+	styleParameters = 'font-size:'+size+'%;font-style:'+style+';color:'+color+';font-weight:'+weight+';text-transform:'+transform+';text-decoration:'+decoration+';';
 
 	console.log(styleParameters);
 
